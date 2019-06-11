2c3bba67ef4149835763cdae48891f0e2a634e77
Ashraful Alom <ashraful@avantikain.com>
2019-06-11 12:18:07 +0530

    Email Attachment

--- application/controllers/admin/Message.php
+++ application/controllers/admin/Message.php
@@ -3,10 +3,9 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Message extends Aipl_admin
-{
-    function __construct()
-    {
+class Message extends Aipl_admin {
+
+    function __construct() {
         parent::__construct();
         $this->isAdminloggedin();
         $this->load->model('message_model');
@@ -14,8 +13,7 @@ class Message extends Aipl_admin
         $this->load->library('breadcrumbs');
     }
 
-    public function index()
-    {
+    public function index() {
         $keyword = '';
         $this->load->library('pagination');
 
@@ -39,17 +37,16 @@ class Message extends Aipl_admin
         );
         $this->breadcrumbs->push('Dashboard', '/admin/dashboard');
         $this->breadcrumbs->push('Message List', '/admin/message');
-        $this->load->view('admin/requires/header',array('title'=>'Message'));
+        $this->load->view('admin/requires/header', array('title' => 'Message'));
         $this->load->view('admin/message/message_list', $data);
         $this->load->view('admin/requires/footer');
     }
 
-    public function search()
-    {
+    public function search() {
         $keyword = $this->uri->segment(3, $this->input->post('keyword', TRUE));
         $this->load->library('pagination');
 
-        if ($this->uri->segment(2)=='search') {
+        if ($this->uri->segment(2) == 'search') {
             $config['base_url'] = base_url() . 'admin/message/search/' . $keyword;
         } else {
             $config['base_url'] = base_url() . 'admin/message/index/';
@@ -59,7 +56,7 @@ class Message extends Aipl_admin
         $config['per_page'] = 10;
         $config['uri_segment'] = 4;
         $config['suffix'] = '.html';
-        $config['first_url'] = base_url() . 'admin/message/search/'.$keyword.'.html';
+        $config['first_url'] = base_url() . 'admin/message/search/' . $keyword . '.html';
         $this->pagination->initialize($config);
 
         $start = $this->uri->segment(4, 0);
@@ -73,138 +70,137 @@ class Message extends Aipl_admin
             'start' => $start,
         );
 
-        $this->load->view('admin/requires/header',array('title'=>'message'));
+        $this->load->view('admin/requires/header', array('title' => 'message'));
         $this->load->view('admin/message/message_list', $data);
         $this->load->view('admin/requires/footer');
     }
 
-    public function read($id){
+    public function read($id) {
         $row = $this->message_model->get_by_id($id);
         if ($row) {
             $data = array(
-		'id' => $row->id,
-		'send_from' => $row->send_from,
-		'send_to' => $row->send_to,
-		'subject' => $row->subject,
-		'message' => $row->message,
-	    );
-		$this->load->view('admin/requires/header',array('title'=>'message'));
-        $this->load->view('admin/message/message_read', $data);
-        $this->load->view('admin/requires/footer');
+                'id' => $row->id,
+                'send_from' => $row->send_from,
+                'send_to' => $row->send_to,
+                'subject' => $row->subject,
+                'message' => $row->message,
+            );
+            $this->load->view('admin/requires/header', array('title' => 'message'));
+            $this->load->view('admin/message/message_read', $data);
+            $this->load->view('admin/requires/footer');
         } else {
             $this->session->set_flashdata('message', 'Record Not Found');
             redirect(site_url('admin/message'));
         }
     }
 
-    public function create()
-    {
+    public function create() {
         $data = array(
             'button' => 'Create',
             'action' => site_url('admin/message/create_action'),
-			'id' => set_value('id'),
-			'send_from' => set_value('send_from'),
-			'send_to' => set_value('send_to'),
-			'subject' => set_value('subject'),
-			'message' => set_value('message'),
-		);
-    $this->breadcrumbs->push('Dashboard', '/admin/dashboard');
-    $this->breadcrumbs->push('Message List', '/admin/message');
-    $this->breadcrumbs->push('Compose Message', '/admin/message/create');
-        $this->load->view('admin/requires/header',array('title'=>'message'));
+            'id' => set_value('id'),
+            'send_from' => set_value('send_from'),
+            'send_to' => set_value('send_to'),
+            'subject' => set_value('subject'),
+            'message' => set_value('message'),
+        );
+        $this->breadcrumbs->push('Dashboard', '/admin/dashboard');
+        $this->breadcrumbs->push('Message List', '/admin/message');
+        $this->breadcrumbs->push('Compose Message', '/admin/message/create');
+        $this->load->view('admin/requires/header', array('title' => 'message'));
         $this->load->view('admin/message/message_form', $data);
         $this->load->view('admin/requires/footer');
     }
 
-    public function create_action()
-    {
-       /* $this->_rules();
+    public function create_action() {
+        /* $this->_rules();
 
-        if ($this->form_validation->run() == FALSE) {
-            $this->create();
-        } else {
-            $data = array(
-		'send_from' => $this->input->post('send_from',TRUE),
-		'send_to' => $this->input->post('send_to',TRUE),
-		'subject' => $this->input->post('subject',TRUE),
-		'message' => $this->input->post('message',TRUE),
-	    );
+          if ($this->form_validation->run() == FALSE) {
+          $this->create();
+          } else {
+          $data = array(
+          'send_from' => $this->input->post('send_from',TRUE),
+          'send_to' => $this->input->post('send_to',TRUE),
+          'subject' => $this->input->post('subject',TRUE),
+          'message' => $this->input->post('message',TRUE),
+          );
 
-            $this->message_model->insert($data);
-            $this->session->set_flashdata('message', 'Create Record Success');
-            redirect(site_url('message'));
-        }
-		*/
+          $this->message_model->insert($data);
+          $this->session->set_flashdata('message', 'Create Record Success');
+          redirect(site_url('message'));
+          }
+         */
 
 
-		$this->load->helper("sendmail");
+        $this->load->helper("sendmail");
         $send_to = $this->input->post('send_to');
         $subject = $this->input->post('subject');
         $message = $this->input->post('message');
-		$data = array(
-			'send_to' => $send_to,
-			'subject' => $subject,
-			'message' => $message,
-	    );
+        
+        if ($this->input->post("upload_download_file")) {
+            $download_file = moveFile(3, $this->input->post("upload_download_file"), "download_file");
+        } else {
+            $download_file = NULL;
+        }//End of if else
+            
+        $data = array(
+            'send_to' => $send_to,
+            'subject' => $subject,
+            'message' => $message,
+        );
 
-		$this->message_model->insert($data);
+        $this->message_model->insert($data);
         $sub = $subject;
-		$msgBody =  $message;
-		//$msgFooter =  "Regards, \n Supply Origin \n Guwahati Assam";
-        $status=sendmail($send_to, $sub, $msgBody);
-		$this->session->set_flashdata('message', 'Email Sent');
+        $msgBody = $message;
+        //$msgFooter =  "Regards, \n Supply Origin \n Guwahati Assam";
+        $status = sendmail($send_to, $sub, $msgBody, $download_file);
+        $this->session->set_flashdata('message', 'Email Sent');
         redirect(site_url('admin/message'));
-
-
-
     }
 
-    public function update($id)
-    {
+    public function update($id) {
         $row = $this->message_model->get_by_id($id);
 
         if ($row) {
             $data = array(
                 'button' => 'Update',
                 'action' => site_url('admin/message/update_action'),
-				'id' => set_value('id', $row->id),
-				'send_from' => set_value('send_from', $row->send_from),
-				'send_to' => set_value('send_to', $row->send_to),
-				'subject' => set_value('subject', $row->subject),
-				'message' => set_value('message', $row->message),
-	    );
+                'id' => set_value('id', $row->id),
+                'send_from' => set_value('send_from', $row->send_from),
+                'send_to' => set_value('send_to', $row->send_to),
+                'subject' => set_value('subject', $row->subject),
+                'message' => set_value('message', $row->message),
+            );
 
-		$this->load->view('admin/requires/header',array('title'=>'message'));
-        $this->load->view('admin/message/message_form', $data);
-		$this->load->view('admin/requires/footer');
-		} else {
+            $this->load->view('admin/requires/header', array('title' => 'message'));
+            $this->load->view('admin/message/message_form', $data);
+            $this->load->view('admin/requires/footer');
+        } else {
             $this->session->set_flashdata('message', 'Record Not Found');
             redirect(site_url('admin/message'));
         }
     }
 
-    public function update_action()
-    {
+    public function update_action() {
         $this->_rules();
 
         if ($this->form_validation->run() == FALSE) {
             $this->update($this->input->post('id', TRUE));
         } else {
             $data = array(
-			'send_from' => $this->input->post('send_from',TRUE),
-			'send_to' => $this->input->post('send_to',TRUE),
-			'subject' => $this->input->post('subject',TRUE),
-			'message' => $this->input->post('message',TRUE),
-			);
+                'send_from' => $this->input->post('send_from', TRUE),
+                'send_to' => $this->input->post('send_to', TRUE),
+                'subject' => $this->input->post('subject', TRUE),
+                'message' => $this->input->post('message', TRUE),
+            );
 
-        $this->message_model->update($this->input->post('id', TRUE), $data);
-        $this->session->set_flashdata('message', 'Update Record Success');
-        redirect(site_url('admin/message'));
+            $this->message_model->update($this->input->post('id', TRUE), $data);
+            $this->session->set_flashdata('message', 'Update Record Success');
+            redirect(site_url('admin/message'));
         }
     }
 
-    public function delete($id)
-    {
+    public function delete($id) {
         $row = $this->message_model->get_by_id($id);
 
         if ($row) {
@@ -217,15 +213,14 @@ class Message extends Aipl_admin
         }
     }
 
-    public function _rules()
-    {
-	$this->form_validation->set_rules('send_from', ' ', 'trim');
-	$this->form_validation->set_rules('send_to', ' ', 'trim');
-	$this->form_validation->set_rules('subject', ' ', 'trim');
-	$this->form_validation->set_rules('message', ' ', 'trim');
+    public function _rules() {
+        $this->form_validation->set_rules('send_from', ' ', 'trim');
+        $this->form_validation->set_rules('send_to', ' ', 'trim');
+        $this->form_validation->set_rules('subject', ' ', 'trim');
+        $this->form_validation->set_rules('message', ' ', 'trim');
 
-	$this->form_validation->set_rules('id', 'id', 'trim');
-	$this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
+        $this->form_validation->set_rules('id', 'id', 'trim');
+        $this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
     }
 
     function get_dtrecords() {
@@ -250,7 +245,7 @@ class Message extends Aipl_admin
         }//End of if else
         $data = array();
         if (!empty($records)) {
-            $slno=1;
+            $slno = 1;
             foreach ($records as $rows) {
                 $id = $rows->id;
                 $subject = $rows->subject;
@@ -264,7 +259,7 @@ class Message extends Aipl_admin
                 $nestedData["send_to"] = $send_to;
                 $nestedData["subject"] = $subject;
                 $nestedData["message"] = $message;
-                $nestedData["id"] = $viewBtn.$editBtn.$deleteBtn;
+                $nestedData["id"] = $viewBtn . $editBtn . $deleteBtn;
                 $data[] = $nestedData;
             }//End of for
         }//End of if
@@ -275,9 +270,12 @@ class Message extends Aipl_admin
             "data" => $data
         );
         echo json_encode($json_data);
-    }//End of get_dtrecords()
+    }
 
-};
+//End of get_dtrecords()
+}
+
+;
 
 /* End of file Message.php */
 /* Location: ./application/controllers/Message.php */
