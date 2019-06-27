50ceb79b256e539036511726c02a47e6da0f685b
sahebul <sahebul99@gmail.com>
2019-06-27 15:16:42 +0530

    template implementation

---
+++ application/views/site/theme1/blogs/blog_details.php
@@ -0,0 +1,86 @@
+<?php $date=date_create($blog_details->created_at);
+ ?>
+ <script>
+ $(document).ready(function(){
+ $('#blogs').addClass('active');
+ });
+ </script>
+ <div class="page-background">
+ <div class="text-center container">
+ <h2 class="header-title-inner-page">Blogs</h2>
+ </div>
+ </div>
+ <br/>
+<div style="margin-top:50px;">
+<div class="container">
+  <div class="row">
+    <div class="col-md-8" style="text-align: justify;">
+      <div class="feat_image">
+        <img src="<?=base_url($blog_details->image);?>" alt="" class="img-fluid">
+      </div>
+      <h5 class="blog_title text-left pt-20"><?=$blog_details->blog_title?></h5>
+      <p class="post_date">  <?php echo date_format($date,"M d,Y H:i"); ?></p>
+      <div class="blog_text_post"><?=$blog_details->blog?></div>
+      <div class="socials">
+        <div class="wt-blog__post__cta__content">
+            <h3>Don't forget to share this post!</h3>
+        <ul class="socials-connected">
+          <?php $share_url=urlencode(base_url()."blogs/".url_title(trim($blog_details->blog_title), '-', TRUE)."/".$blog_details->blogs_id); ?>
+          <li><a href="http://www.facebook.com/sharer.php?u=<?php echo $share_url;?>" target="_blank"><i class="fa fa-facebook-official"></i></a>&nbsp;&nbsp;&nbsp;</li>
+          <li><a  href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo $share_url; ?>" target="_blank"><i class="fa fa-linkedin-square"></i></a></li>
+        </ul>
+      </div>
+    </div>
+    </div>
+    <div class="col-md-4">
+      <div class="search-container">
+        <form action="<?=base_url();?>search" method="post">
+          <!-- <input type="text" placeholder="Search.." name="search">
+          <button type="submit"><i class="fa fa-search"></i></button> -->
+          <div class="row">
+            <div class="col-sm-11">
+              <div class="form-group">
+                <input class="form-control " style="position:relative;"  type="text" required placeholder="Search.." name="search">
+                <button style="position:absolute; right: 0px;top: 0px;"  class="fa fa-search form-control-feedback" type="submit"></button>
+              </div>
+            </div>
+            <div class="col-sm-1">
+            </div>
+          </div>
+
+        </form>
+      </div>
+      <div class="section-title">
+				<h2>Recent Posts</h2>
+			</div>
+      <?php foreach ($recent_blogs as $key => $recent): ?>
+        <div class="post post-thumb">
+                  <a class="post-img" href="<?=base_url()?>blogs/<?=url_title(trim($recent->blog_title), '-', TRUE)?>/<?=$recent->blogs_id;?>">
+                    <?php if($recent->image){ ?>
+                      <img src="<?=base_url($recent->image)?>" alt="">
+                    <?php }else { ?>
+                      <img src="<?=base_url("assets/images/not-found.png")?>"  alt="">
+                    <?php } ?>
+
+                  </a>
+                  <div class="post-body">
+                    <h3 class="post-title"><a href="<?=base_url()?>blogs/<?=url_title(trim($recent->blog_title), '-', TRUE)?>/<?=$recent->blogs_id;?>"><?=$recent->blog_title;?></a></h3>
+                    <div class="post-meta">
+                      <span class="post-date"><?php $blogdate=date_create($recent->created_at); echo "Posted on ".date_format($blogdate,"M d,Y H:i");?></span>
+                    </div>
+                  </div>
+        </div>
+      <?php endforeach; ?>
+      <div class="tags-widget">
+				<ul>
+          <?php if($tags){
+            foreach ($tags as $key => $value) {?>
+              	<li><a href="<?=base_url()?>blogs/<?=$value?>"><?=$value?></a></li>
+            <?php }
+          }?>
+				</ul>
+		 </div>
+    </div>
+  </div>
+</div>
+</div>
---
+++ application/views/site/theme1/requires/new_footer.php
@@ -0,0 +1,208 @@
+
+
+<div class="footer-wrap footer-v3" id="footer">
+           <footer>
+               <div class="container">
+                   <div class="row">
+                       <div class="col-sm-6">
+                           <section class="widget">
+                             <div class="widget-heading">
+                                 <h4>To Avail our Services</h4>
+                             </div>
+                                 <form action="<?=base_url('contactus/save')?>" method="post" class="form-footer">
+                                     <div class="row">
+                                         <div class=" col-md-6 form-group">
+                                             <input  type="text" class="form-control input-sm." name="fullname" placeholder="Full Name" />
+                                             <?=form_error("fullname")?>
+                                         </div>
+                                         <div class="col-md-6 form-group">
+                                             <input  type="text" class="form-control input-sm." name="email" autocomplete="off" placeholder="Email id" />
+                                             <?=form_error("email")?>
+                                         </div>
+                                     </div>                    <div class="row">
+                                         <div class=" col-md-6 form-group">
+                                             <input  type="text" class="form-control input-sm." name="mobile" maxlength="10" placeholder="Mobile No." />
+                                             <?=form_error("mobile")?>
+                                         </div>
+                                         <div class="col-md-6 form-group">
+                                             <input  type="text" class="form-control input-sm." name="company" placeholder="Company name" />
+                                             <?=form_error("company")?>
+                                         </div>
+                                     </div>
+
+                                     <div class="row">
+                                         <div class=" col-md-12 form-group">
+                                             <textarea class="form-control" name="message" rows="2" placeholder="Type your message here.."></textarea>
+                                             <?=form_error("message")?>
+                                         </div>
+                                     </div>
+                                     <button type="submit" class="btn btn-primary btn-lg mybtn-footer-form">Send Message</button>
+                                 </form>
+                           </section>
+                       </div>
+
+                       <div class="col-sm-3">
+                           <section class="widget v-recent-entry-widget">
+                               <div class="widget-heading">
+                                   <h4>Recent Posts</h4>
+                               </div>
+                               <?php
+                               $recent_blogs=$this->blogs_model->get_recent_blogs();
+                               if($recent_blogs){ ?>
+                                 <ul>
+                                   <?php foreach ($recent_blogs as $key => $recent): ?>
+                                     <li>
+                                         <a href="<?=base_url()?>blogs/<?=url_title(trim($recent->blog_title), '-', TRUE)?>/<?=$recent->blogs_id;?>"><?=$recent->blog_title?></a>
+                                         <span class="post-date"><?php $blogdate=date_create($recent->created_at); echo date_format($blogdate,"M d,Y H:i");?></span>
+                                     </li>
+                                   <?php endforeach; ?>
+
+
+                                 </ul>
+                               <?php }?>
+
+                           </section>
+                       </div>
+                       <div class="col-sm-3">
+                           <section class="widget">
+                               <div class="widget-heading">
+                                   <h4>Contact Us</h4>
+                               </div>
+                               <div class="footer-contact-info">
+                                   <ul>
+                                       <li>
+                                           <p><i class="fa fa-building"></i>Supply Origin </p>
+                                       </li>
+                                       <li>
+                                           <p><i class="fa fa-map-marker"></i>G.S. Road,Guwahati, Assam, India </p>
+                                       </li>
+                                       <li>
+                                           <p><i class="fa fa-envelope"></i><strong>Email:</strong> <a href="mailto:sales@supplyorigin.com">sales@supplyorigin.com</a></p>
+                                       </li>
+                                       <li>
+                                           <p><i class="fa fa-phone"></i><strong>Phone:</strong> +91- 9706189034</p>
+                                       </li>
+                                       <li>
+                                           <p><a href="<?=base_url('home/feedback');?>">Send Feedback</a></p>
+                                       </li>
+                                   </ul>
+                                   <br />
+
+                                   <div class="clearfix pt-2">
+                                       <a href="#" class="social-icon si-dark si-facebook" title="Facebook">
+                                           <i class="si-icon-facebook"></i>
+                                           <i class="si-icon-facebook"></i>
+                                       </a>
+                                       <a href="#" class="social-icon si-dark si-twitter" title="Twitter">
+                                           <i class="si-icon-twitter"></i>
+                                           <i class="si-icon-twitter"></i>
+                                       </a>
+                                       <a href="#" class="social-icon si-dark si-instagram" title="Instagram">
+                                           <i class="si-icon-instagram"></i>
+                                           <i class="si-icon-instagram"></i>
+                                       </a>
+                                       <a href="#" class="social-icon si-dark si-vk" title="Vk">
+                                           <i class="si-icon-vk"></i>
+                                           <i class="si-icon-vk"></i>
+                                       </a>
+                                   </div>
+                               </div>
+                           </section>
+                       </div>
+                   </div>
+               </div>
+           </footer>
+
+           <div class="copyright">
+               <div class="container">
+                   <p>© <?php echo date("Y"); ?> Supply Origin. All rights reserved.</p>
+                   <nav class="footer-menu std-menu">
+                       <ul class="menu">
+                           <li><a href="<?=base_url();?>home/about">About Us</a></li>
+                           <li><a href="<?=base_url();?>blogs">Blog</a></li>
+                           <li><a href="<?=base_url();?>home/contact">Contact</a></li>
+                       </ul>
+                   </nav>
+               </div>
+           </div>
+       </div>
+       <!--End Footer-Wrap-->
+   </div>
+
+   <div class="fw-slider-spacer"></div>
+
+   <!-- Libs -->
+
+   <script src="<?= base_url(); ?>assets/template/js/popper.js"></script>
+   <script src="<?= base_url(); ?>assets/template/js/bootstrap.min.js"></script>
+   <script src="<?= base_url(); ?>assets/template/js/jquery.flexslider-min.js"></script>
+   <script src="<?= base_url(); ?>assets/template/js/jquery.easing.js"></script>
+   <script src="<?= base_url(); ?>assets/template/js/jquery.fitvids.js"></script>
+   <script src="<?= base_url(); ?>assets/template/js/jquery.carouFredSel.min.js"></script>
+   <script src="<?= base_url(); ?>assets/template/js/jquery.validate.js"></script>
+   <script src="<?= base_url(); ?>assets/template/js/theme-plugins.js"></script>
+   <script src="<?= base_url(); ?>assets/template/js/jquery.isotope.min.js"></script>
+   <script src="<?= base_url(); ?>assets/template/js/imagesloaded.js"></script>
+   <script src="<?= base_url(); ?>assets/template/js/view.min9df2.js?auto"></script>
+   <script src="<?= base_url(); ?>assets/template/plugins/aos/aos.js"></script>
+
+   <script src="<?= base_url(); ?>assets/template/plugins/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+   <script src="<?= base_url(); ?>assets/template/plugins/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+   <script src="<?= base_url(); ?>assets/template/js/theme-core.js"></script>
+   <script src="<?= base_url(); ?>assets/template/js/theme.js"></script>
+   <script src="<?= base_url(); ?>assets/template/js/theme.init.js"></script>
+</body>
+
+<!-- Mirrored from bootstraptemplates.net/vertex/index-11.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 24 Jun 2019 10:30:54 GMT -->
+</html>
+
+<?php $this->load->view("site/requires/cart_modal.php"); ?>
+<?php $this->load->view("site/requires/login_modal.php"); ?>
+<?php $this->load->view("site/requires/register_modal.php"); ?>
+
+<script type="text/javascript" src="<?= base_url(); ?>assets/js/move-top.js"></script>
+<script type="text/javascript" src="<?= base_url(); ?>assets/js/easing.js"></script>
+
+<script type="text/javascript">
+//console.log("d sj");
+    $(document).ready(function () {
+        //alert("jj");
+        $(document).on("click", ".cart_modal", function () {
+
+            $('#cart_modal').modal("show");
+        });
+        $(document).on("click", ".login_modal", function () {
+            $('#login_modal').modal("show");
+        });
+        $(document).on("click", ".register_modal", function () {
+            $('#register_modal').modal("show");
+        });
+        $(".scroll").click(function (event) {
+            event.preventDefault();
+            $('html,body').animate({
+                scrollTop: $(this.hash).offset().top
+            }, 1000);
+        });
+        var password = document.getElementById("password");
+        var cpassword = document.getElementById("cpassword");
+        function validatePassword() {
+            if (password.value != cpassword.value) {
+                cpassword.setCustomValidity("Passwords Don't Match");
+            } else {
+                cpassword.setCustomValidity('');
+            }
+        }
+        password.onchange = validatePassword;
+        cpassword.onkeyup = validatePassword;
+    });
+
+    function clear_cart() {
+        var result = confirm('Are you sure want to clear cart?');
+        if (result) {
+            window.location = "<?php echo base_url(); ?>/shopping/remove/all";
+        } else {
+            return false; // cancel button
+        }
+    }
+</script>
