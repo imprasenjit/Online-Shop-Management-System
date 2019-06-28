9ea5f38a9a6e000eaeb47f8793bbd530d52b5cdc
Prasenjit <prasenjit@avantikain.com>
2019-06-28 16:07:10 +0530

    product form

--- application/views/admin/products/products_form.php
+++ application/views/admin/products/products_form.php
@@ -6,24 +6,12 @@
         <div class="col-xl-12 col-lg-12">
             <div class="card shadow mb-4">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary"><?php echo $button ?>Product</h6>
-                    <div class="dropdown no-arrow">
-                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                            <!--<div class="dropdown-header">Dropdown Header:</div>
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">Something else here</a>
--->
-                        </div>
-                    </div>
+                    <h6 class="m-0 font-weight-bold text-primary"><?php echo $button ?> Product</h6>
                 </div>
                 <!-- Card Body -->
+                <form class="form-horizontal" action="<?php echo $action; ?>" method="post" id="myform">
                 <div class="card-body">
-                    <form class="form-horizontal" action="<?php echo $action; ?>" method="post" id="myform">
+                   
                         <div class="row">
                             <!-- <div class="form-group full-width"> -->
                             <div class=" col-md-6">
@@ -151,14 +139,12 @@
                                 </table>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-12" align="center">
+                        </div>
+                        <div class="card-footer">
                                 <input type="hidden" name="id" value="<?php echo $id; ?>" />
                                 <a href="<?php echo site_url('admin/products') ?>" class="btn btn-sm btn-info">Cancel</a>
                                 <button type="submit" class="btn btn-sm btn-primary">Submit</button>
                             </div>
-                            <br>
-                        </div>
                     </form>
                 </div>
             </div>
